260d1c3206057cb05b46f3063f04ded8b626160d
Mia Tyzzer <mia@parksidesecurities.com>
2019-04-12 13:48:10 -0700

    deploy snapshot version for testing purposes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject parkside-securities/hydrox "0.10.2"
+(defproject parkside-securities/hydrox "0.10.3-SNAPSHOT"
   :description "dive deeper into your code"
   :url "https://github.com/helpshift/hydrox"
   :license {:name "Eclipse Public License"
